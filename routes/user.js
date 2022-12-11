c7a031b2987781741fad87ce29acaff084b39fe3
Md. Sakhawat Hossain <biplobbms@gmail.com>
2022-12-11 18:05:20 +0600

    all models created done and also added 3 routes but not work yet

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-router.get('/usertest', (req, res) => {
-	res.send('User test is successful!');
-});
-router.post('/userposttest', (req, res) => {
-	const username = req.body.username;
-	res.send('Your username is: ' + username);
-});
+// router.get('/usertest', (req, res) => {
+// 	res.send('User test is successful!');
+// });
+// router.post('/userposttest', (req, res) => {
+// 	const username = req.body.username;
+// 	res.send('Your username is: ' + username);
+// });
 
 module.exports = router;
