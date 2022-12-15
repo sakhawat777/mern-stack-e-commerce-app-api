2f46ca23ae4e121e03140ec722fdb06b1e9d08a7
Md. Sakhawat Hossain <biplobbms@gmail.com>
2022-12-15 13:56:21 +0600

    Login API worked sucessfuly

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 //REGISTER
 router.post('/register', async (req, res) => {
@@ -9,7 +10,7 @@ router.post('/register', async (req, res) => {
 		email: req.body.email,
 		password: CryptoJS.AES.encrypt(
 			req.body.password,
-			process.env.PASS_SECRET
+			process.env.PASS_SEC
 		).toString(),
 	});
 
@@ -20,4 +21,42 @@ router.post('/register', async (req, res) => {
 		res.status(500).json(err);
 	}
 });
+//LOGIN
+
+router.post('/login', async (req, res) => {
+	try {
+		const user = await User.findOne({
+			userName: req.body.user_name,
+		});
+
+		!user && res.status(401).json('Wrong User Name');
+
+		const hashedPassword = CryptoJS.AES.decrypt(
+			user.password,
+			process.env.PASS_SEC
+		);
+
+		const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+		const inputPassword = req.body.password;
+
+		originalPassword != inputPassword &&
+			res.status(401).json('Wrong Password');
+
+		const accessToken = jwt.sign(
+			{
+				id: user._id,
+				isAdmin: user.isAdmin,
+			},
+			process.env.JWT_SEC,
+			{ expiresIn: '3d' }
+		);
+
+		const { password, ...others } = user._doc;
+		res.status(200).json({ ...others, accessToken });
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 module.exports = router;
